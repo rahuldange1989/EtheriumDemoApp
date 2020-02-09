15ecb6122227b5f869a1caf4ac8fdf015db750c6
Rahul <rahul.dange1989@gmail.com>
2020-02-09 13:19:47 +0100

    Created basic project structure.

---
+++ EtheriumDemoApp/EtheriumDemoApp/Setup/SetupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SetupViewController.swift
+//  EtheriumDemoApp
+//
+//  Created by Rahul Dange on 09/02/20.
+//  Copyright © 2020 Rahul Dange. All rights reserved.
+//
+
+import UIKit
+
+class SetupViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
---
+++ EtheriumDemoApp/Pods/Target Support Files/Pods-EtheriumDemoApp/Pods-EtheriumDemoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EtheriumDemoApp : NSObject
+@end
+@implementation PodsDummy_Pods_EtheriumDemoApp
+@end
